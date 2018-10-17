8c89ed386b4686882b03a1fa3cd187fd8e6206c4
americanman <snowboardracer7@gmail.com>
2018-10-17 19:26:30 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ProgressBar } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ProgressBar striped bsStyle="success" now={100} />
+        <h2>this is a section with componentes <br /> from reac-bootstrap npm package</h2>
+        <ProgressBar striped bsStyle="success" now={100} />
+        <ProgressBar active now={77} />;
+        <h2>this is a section build with bootstrap <br /> and converted to components</h2>
+        <ProgressBar active now={77} />;
       </div>
     );
   }
