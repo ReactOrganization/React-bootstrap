93d5760133e237a6c1725046095a23d70399b26e
americanman <snowboardracer7@gmail.com>
2018-10-17 21:28:37 -0400

    Add fluid container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import { ProgressBar } from 'react-bootstrap'
+import ThreeColumns from './components/three-columns'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ProgressBar striped bsStyle="success" now={100} />
-        <h2>this is a section with componentes <br /> from reac-bootstrap npm package</h2>
+          <h2>this is a section with componentes <br /> from reac-bootstrap npm package</h2>
         <ProgressBar striped bsStyle="success" now={100} />
         <ProgressBar active now={77} />;
-        <h2>this is a section build with bootstrap <br /> and converted to components</h2>
+          <h2>this is a section build with bootstrap <br /> and converted to components</h2>
+          <ThreeColumns />
         <ProgressBar active now={77} />;
       </div>
     );
